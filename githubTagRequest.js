2cd153fc982a4064b3d380cb255656249c840ce6
Judah Mantell <40009793+MidnightCoffeeInc@users.noreply.github.com>
2021-03-02 12:10:05 -0500

    Deployed 5eeacae with MkDocs version: 1.1.2

--- githubTagRequest.js
+++ githubTagRequest.js
@@ -1,8 +1,34 @@
-const searchQueryURL = 'https://github.com/MidnightCoffeeInc/SceneForge-Alpha-Test';
+function requestUserRepos(){
+    
+    // Create new XMLHttpRequest object
+    const xhr = new XMLHttpRequest();
+    
+    // GitHub endpoint, dynamically passing in specified username
+    const url = `https://github.com/MidnightCoffeeInc/SceneForge-Alpha-Test/`;
+    
+    // Open a new connection, using a GET request via URL endpoint
+    // Providing 3 arguments (GET/POST, The URL, Async True/False)
+    xhr.open('GET', url, true);
+    
+    // When request is received
+    // Process it here
+    xhr.onload = function() {
+    
+        // Parse API data into JSON
+        const data = JSON.parse(this.response);
+        
+        // Log the response
+        console.log(data);
+        
+        // Loop over each object in data array
+        console.log(data.name);
 
-    window.addEventListener('DOMContentLoaded', function(e) {
-      return fetch(searchQueryURL)
-      .then(result => result.json())
-      .then(response => console.log(response))
-      .catch(err => console.log(err))
-    });
+    }
+    
+    // Send the request to the server
+    xhr.send();
+    
+}
+
+// Call function passing in 'facebook' as GitHub username
+requestUserRepos();
